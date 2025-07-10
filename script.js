40ac20dfe8d197e9029ffc66bb19ff1aade359b7
rensdv06 <rensdv5@gmail.com>
2025-07-10 23:03:25 +0200

    Fix eventAction and delete context console.log() Because this should be the correct value

--- script.js
+++ script.js
@@ -88,12 +88,10 @@ function completeLastTimeLog(issueBodyLines, locale) {
 }
 
 function main(github, context) {
-  console.log(JSON.stringify(context));
-
   const issueBodyLines = context.payload.issue.body.split("\n");
   const LOCALE = "sv-SE"; // YYYY-MM-DD HH:MM:SS format
 
-  const eventAction = context.payload.event.action;
+  const eventAction = context.payload.action;
   let updatedIssueBodyLines;
   if (eventAction === "labeled") {
     updatedIssueBodyLines = addTimeLog(issueBodyLines, LOCALE);
